7fd23a4309e0c8bdacdb254a76d215c7039ca033
Pritesh Thorat <thoratpritesh1@gmail.com>
2026-05-28 16:35:03 +0530

    Add ChaiVarityForm and update chai_store_view to handle form submissions

--- chai/urls.py
+++ chai/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_chai, name='all_chai'),
+    path('chai-stores/', views.chai_store_view, name='chai_stores')
 ]
--- chai/views.py
+++ chai/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import ChaiVarityForm
+from .models import Store
 
 # Create your views here.
 def all_chai(req):
     return render(req, 'chai/all_chai.html')
+
+def chai_store_view(req):
+    stores = None
+    if req.method == 'POST':
+        form = ChaiVarityForm(req.POST)
+        
+        if form.is_valid():
+            chai_varity = form.cleaned_data['chai_varity']
+            stores = Store.objects.filter(chai_varieties=chai_varity)
+    else:
+        form = ChaiVarityForm()
+
+    return render(req, 'chai/chai_stores.html', {'form': form, 'stores': stores})
